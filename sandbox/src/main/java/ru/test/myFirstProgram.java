3ff462f71d85dd8c2c6818f59d7fdaf046dd3300
Sintsev <105670038+Sintsev@users.noreply.github.com>
2022-07-20 18:35:59 +0300

    new object and class

--- sandbox/src/main/java/ru/test/myFirstProgram.java
+++ sandbox/src/main/java/ru/test/myFirstProgram.java
@@ -4,10 +4,18 @@ public class myFirstProgram
 	{
 		public static void main(String [] agrs)
 		{
-		System.out.println("Hello, world!!!!");
-			int l = 6;
-			int s = (l*l);
-			System.out.println("Площадь квадрата со стороной " + l +  " = " + s);
+			hello("user");
+			hello("world");
+			hello("Alexey");
+
+				Square s = new Square(5);
+			System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+				Rectangle r = new Rectangle(4,6);
+			System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 		}
-	
+		public static void hello(String somebody){
+			System.out.println("Hello, " + somebody + "!");
+		}
+
+
 	}
